f7f24ed530b443a0eb74d2f94d936f785ede439b
Matt Callaghan <mcallaghan@naperville203.org>
2026-02-10 14:03:51 -0600

    Wrote starter note on file io

--- NameAnalyzer.java
+++ NameAnalyzer.java
@@ -68,11 +68,32 @@ public class NameAnalyzer
         reader.close();
     }
     
+    /**
+     * Uses a simple search algorithm to find a NameRecord by name and print it.
+     */
+    public void printCountByName(String name)
+    {
+        for(NameRecord rec : boyRecords)
+        {
+            String recordName = rec.getName();
+            // equalsIgnoreCase is an alternative version of the equals method that is case insensitive
+            if (recordName.equalsIgnoreCase(name))
+            {
+                System.out.println(rec);
+                return;  // early return
+            }
+        }
+        System.out.println("Name is not found: " + name);
+    }
+    
     
     public static void main() throws IOException
     {
         NameAnalyzer app = new NameAnalyzer();
-        app.loadFile("babynames2010s.txt");
+        app.loadFile("babynames2000s.txt");
+        app.printCountByName("Matthew");
+        app.printCountByName("Hayes");
+        app.printCountByName("Connor");
         
     }
     
